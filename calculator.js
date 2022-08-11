e04e9781553412c50ce105b045b2b762fbf08a30
saikab <97691842+saikab@users.noreply.github.com>
2022-08-11 19:52:00 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,32 @@
+var a=parseInt(prompt("Please put first number:"));
+var b=parseInt(prompt("Please put second number:"));
+
+var x= (a+b);
+var y= (a-b);
+var z= (a/b);
+var s= (a%b);
+var p= (a*b);
+var q= (a**b);
+
+
+
+
+
+document.write("Adittion: "+ x + "<br/>");
+
+document.write("Substraction: "+ y + "<br/>");
+
+document.write("Division:" + z + "<br/>");
+document.write("remainder:"+ s + "<br/>");
+
+document.write("Mulplication:" + p + "<br/>"  );
+
+
+document.write("To the power:"+ q + "<br/>" );
+
+
+
+
+
+
+
